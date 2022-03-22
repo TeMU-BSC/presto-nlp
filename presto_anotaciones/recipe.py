ad31fae0cb405d96e313609d2d9076bce17b33c3
Casimiro Pio Carrino <c.casimiro88@gmail.com>
2022-03-22 09:41:06 +0100

    remove the "no" class examples from second-level annotation

--- presto_anotaciones/recipe.py
+++ presto_anotaciones/recipe.py
@@ -4,18 +4,22 @@ from typing import List, Optional
 from prodigy.util import split_string
 
 hierarchy = {'yes': ['sobregeneralización', 'leer la mente', 'imperativos', 'etiquetado',
-                     'pensamiento absolutista', 'adivinación', 'catastrofismo', 'abstracción selectiva'
+                     'pensamiento absolutista', 'adivinación', 'catastrofismo', 'abstracción selectiva',
                      'razonamiento emocional', 'personalización'], 'no': ['no hay distorsión']}
 
-def get_stream(examples): # from https://support.prodi.gy/t/does-prodigy-supports-hierarchical-annotation/1249/9
+# from https://support.prodi.gy/t/does-prodigy-supports-hierarchical-annotation/1249/9
+
+
+def get_stream(examples):
     for eg in examples:   # the examples with top-level categories
         top_labels = eg['accept']  # ['A'] or ['B', 'C'] if multiple choice
         for label in top_labels:
-            sub_labels = hierarchy[label]
-            options = [{'id': opt, 'text': opt} for opt in sub_labels]
-            # create new example with text and sub labels as options
-            new_eg = {'text': eg['text'], 'options': options}
-            yield new_eg
+            if label != "no":
+                sub_labels = hierarchy[label]
+                options = [{'id': opt, 'text': opt} for opt in sub_labels]
+                # create new example with text and sub labels as options
+                new_eg = {'text': eg['text'], 'options': options}
+                yield new_eg
 
 # Helper functions for adding user provided labels to annotation tasks.
 # def add_label_options_to_stream(stream, labels):
@@ -38,7 +42,7 @@ def get_stream(examples): # from https://support.prodi.gy/t/does-prodigy-support
     exclusive=("Treat classes as mutually exclusive", "flag", "E", bool),
     exclude=("Names of datasets to exclude", "option", "e", split_string),
 )
-def textcat_modified( # from https://github.com/explosion/prodigy-recipes/blob/master/textcat/textcat_manual.py
+def textcat_modified(  # from https://github.com/explosion/prodigy-recipes/blob/master/textcat/textcat_manual.py
     dataset: str,
     source: str,
     #label: Optional[List[str]] = None,
@@ -56,7 +60,7 @@ def textcat_modified( # from https://github.com/explosion/prodigy-recipes/blob/m
     # dictionary for each example in the data.
     stream = JSONL(source)
 
-    #Add labels to each task in stream
+    # Add labels to each task in stream
     # has_options = len(label) > 1
     # if has_options:
     #     stream = add_label_options_to_stream(stream, label)
@@ -70,12 +74,14 @@ def textcat_modified( # from https://github.com/explosion/prodigy-recipes/blob/m
     new_stream = get_stream(stream)
 
     return {
-        "view_id": "choice" if has_options else "classification",  # Annotation interface to use
+        # Annotation interface to use
+        "view_id": "choice" if has_options else "classification",
         "dataset": dataset,  # Name of dataset to save annotations
         "stream": new_stream,  # Incoming stream of examples
         "exclude": exclude,  # List of dataset names to exclude
         "config": {  # Additional config settings, mostly for app UI
-            "choice_style": "single" if exclusive else "multiple", # Style of choice interface
-            "exclude_by": "input" if has_options else "task", # Hash value used to filter out already seen examples
+            "choice_style": "single" if exclusive else "multiple",  # Style of choice interface
+            # Hash value used to filter out already seen examples
+            "exclude_by": "input" if has_options else "task",
         },
     }
