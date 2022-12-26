234a51b45edb670af3df2864d2f6e792eabd8a3f
Yonah Forst <yonah.forst@idt.net>
2022-12-26 15:12:47 +0100

    support different event topics per environment

--- rasa/vickybot/event_broker.py
+++ rasa/vickybot/event_broker.py
@@ -14,18 +14,22 @@ class SNSEventBroker(EventBroker):
 
     def __init__(
         self,
-        topicArn,
+        topicArns,
     ) -> None:
         self.snsClient = boto3.client('sns')
-        self.topicArn = topicArn
+        self.topicArns = topicArns
 
     @classmethod
     async def from_endpoint_config(cls, config) -> "SNSEventBroker":        
         """Creates broker. See the parent class for more information."""
-        return cls(topicArn=config.url, **config.kwargs)
+        return cls(topicArns=config.url, **config.kwargs)
 
     def publish(self, event: Dict[Text, Any]) -> None:
-        self.snsClient.publish(
-            TopicArn=self.topicArn,
-            Message=json.dumps(event),
-        )
+        logger.info(event)
+        stage = event.get('sender_id').split('@')[1]
+        topicArn = self.topicArns.get(stage)
+        if topicArn:
+            self.snsClient.publish(
+                TopicArn=topicArn,
+                Message=json.dumps(event),
+            )
